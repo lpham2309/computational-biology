c0694726d26b03d9d9740d1cafb22d32688e3a1d
Lam Pham <lam.pham@tufts.edu>
2022-02-28 21:31:21 -0500

    continue updating

--- HW02/gibbs.py
+++ HW02/gibbs.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 def main(argv):
     input_sequences = []
@@ -9,6 +10,7 @@ def main(argv):
             row = line.replace("\n", "")
             curr_input.append(row)
     
+    # print(curr_input)
     # Assume we have 6 sequences with same length, randomize a starting position
     sequence_length = len(curr_input[0])
     initial_site = random.randrange(0, sequence_length, 1)
@@ -16,7 +18,16 @@ def main(argv):
     # Pick a s* to remove
     random_sequence_index = random.randrange(0, len(curr_input), 1)
     curr_input.remove(curr_input[random_sequence_index])
-
+    # print(curr_input)
+    
     propensityMatrix = []
-    for i in range(len())
-
+    residues = ["A", "T", "C", "G"]
+    for i in range(len(residues)):
+        subMatrix = []
+        for j in range(len(curr_input)):
+            subMatrix.append(0)
+        propensityMatrix.append(subMatrix)
+    
+    
+if __name__=='__main__':
+    main(sys.argv)
